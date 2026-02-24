f7f2b35b95edefdc408bb295d82210b967672676
Banu Begum <banusssi056@gmail.com>
2026-02-24 11:00:12 +0530

    fix: disable webpack cache to stay under cloudflare 25mb limit

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,11 +2,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    trailingSlash: true,
     typescript: {
         ignoreBuildErrors: true,
     },
     eslint: {
         ignoreDuringBuilds: true,
+    },
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            config.cache = false;
+        }
+        return config;
     },
 };
 
